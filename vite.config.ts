d2c879f8cc5433a2134e042be3e72339bc5c2f93
ewinwidiyan02-dot <ewinwidiyan02@gmail.com>
2026-05-11 14:47:51 +0800

    fix: adjust vite plugin order for tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackRouter(),
+    tanstackStart(),
     tsconfigPaths(),
     react(),
     tailwindcss(),
-    tanstackStart(),
   ],
   build: {
     chunkSizeWarningLimit: 1000,
